bee7d4eefef2befc61621fb1e9375cdfd49dfc44
Aditya Sinha <adityasinha3201@gmail.com>
2023-04-14 00:05:24 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,46 @@
+import backend from "./backend.png";
+import cs from "./cs.png";
+import bhaskar from "./bhaskar.jpg";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import next from "./tech/next.png";
+import css from "./tech/css.png";
+
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  backend,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  carrent,
+  jobit,
+  tripguide,
+  next,
+  cs,
+  bhaskar,
+};
